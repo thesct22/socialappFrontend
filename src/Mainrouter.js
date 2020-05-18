63e73318242eb545427c8aeee1914de966e243f4
thesct22 <sharathct22@gmail.com>
2020-05-18 16:49:06 +0530

    137 added private routing

--- src/Mainrouter.js
+++ src/Mainrouter.js
@@ -7,6 +7,7 @@ import Signin from "./user/Signin";
 import Profile from "./user/Profile";
 import Users from "./user/Users";
 import EditProfile from "./user/EditProfile";
+import PrivateRoute from "./auth/PrivateRoute";
 
 const MainRouter=()=>(
     <div>
@@ -15,9 +16,9 @@ const MainRouter=()=>(
             <Route exact path="/" component={Home}/>
             <Route exact path="/signup" component={Signup}/>
             <Route exact path="/signin" component={Signin}/>
-            <Route exact path="/user/:userId" component={Profile}/>
+            <PrivateRoute exact path="/user/:userId" component={Profile}/>
             <Route exact path="/users" component={Users}/>
-            <Route exact path ="/user/edit/:userId" component={EditProfile}/>
+            <PrivateRoute exact path ="/user/edit/:userId" component={EditProfile}/>
             
         </Switch>
     </div>
---
+++ src/auth/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import {isAuthenticated} from './Index'
+
+const PrivateRoute=({component: Component, ...rest})=>(
+    //props means the components passed down to this private route component
+    <Route {...rest} render={props=>isAuthenticated()?(
+        <Component {...props}/>
+        
+    ):(
+        <Redirect to={{
+            pathname:"/signin",
+            state:{from:props.location}
+        }}/>
+    )}/>
+)
+
+export default PrivateRoute;
